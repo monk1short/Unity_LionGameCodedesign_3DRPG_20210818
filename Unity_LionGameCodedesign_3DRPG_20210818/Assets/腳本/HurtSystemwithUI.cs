096577f48614e45900fab0f9f69516cf81763bcc
monk1short <89112674+monk1short@users.noreply.github.com>
2021-11-24 11:59:47 +0800

    1124上午上課內容

---
+++ Unity_LionGameCodedesign_3DRPG_20210818/Assets/腳本/HurtSystemwithUI.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 包含介面的受傷系統
+/// 可以處裡血條更新
+/// </summary>
+public class HurtSystemwithUI : HurtSystem
+{
+    [Header("要更新的血條")]
+    public Image imgHp;
+
+    /// <summary>
+    /// 血條效果專用的扣血前血量
+    /// </summary>
+    private float hpEffectOriginal;
+
+    // 複寫父類別成員 override
+    public override void Hurt(float damage)
+    {
+        // 該成員的父類別基底 父類別內的內容
+        base.Hurt(damage);
+
+        StartCoroutine(HpBarEffect());
+        //imgHp.fillAmount = hp / hpMax;
+    }
+
+    /// <summary>
+    /// 血條效果
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator HpBarEffect()
+    {
+        while (hpEffectOriginal != hp)                      // 當 扣血前血量不等於血量
+        {
+            hpEffectOriginal --;                            // 遞減
+            imgHp.fillAmount = hpEffectOriginal / hpMax;    // 更新血條
+            yield return new WaitForSeconds(0.01f);         // 等待
+        }
+    }
+}
